b161b2a537a60916f88a8ad9b3baadb370695093
Adrian Żak <adrian.damian.zak@gmail.com>
2024-04-29 20:13:56 +0200

    Added angular project

---
+++ Street.Lightning.UI/src/app/app.routes.ts
@@ -0,0 +1,6 @@
+import { Routes } from '@angular/router';
+import { AddCityComponent } from './add-city/add-city.component';
+
+export const routes: Routes = [
+   { path: 'add-city', component: AddCityComponent }
+];
